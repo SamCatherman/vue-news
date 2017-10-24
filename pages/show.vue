b4dd0a2f5fb1bfdd404a75edf0ab7a69cd9b480b
SamCatherman <sam.catherman@gmail.com>
2017-10-24 14:44:34 -0400

    adds pages for jobs, show

---
+++ pages/show.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 3)display items component -->
+  <div>
+    <h1>Show</h1>
+    <Items>
+    </Items>
+  </div>
+</template>
+<script>
+  import Items from "~/components/Items.vue"
+  //1)index page loads Items component
+  export default {
+    components: {
+      Items
+    },
+//2)load items into the store by dispatching LOAD_ITEMS action, with payload of topstories.json
+    async fetch({ store }){
+      await store.dispatch("LOAD_ITEMS", "showstories.json")
+    }
+  }
+</script>
